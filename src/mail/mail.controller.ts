d2109703ec2d3156416e741463feb923e2055c2b
Rath <theangrathana@Rath.local>
2025-10-20 18:08:36 +0700

    upd

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -1,17 +1,19 @@
 // src/mail/mail.controller.ts
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { MailService } from './mail.service';
+import { Public } from 'src/auth/guard/public.decorator';
 
 @Controller('mail')
 export class MailController {
   constructor(private readonly mail: MailService) { }
 
 
-  @Post('test')
-  async test(@Body() body: { to: string; subject?: string }) {
+  @Public()
+  @Get('test')
+  async test() {
     return this.mail.send({
-      to: body.to,
-      subject: body.subject ?? 'Test via Gmail SMTP',
+      to: 'theangrathana1@gmail.com',
+      subject: 'Test via Gmail SMTP',
       html: '<p>Hello from NestJS via Gmail SMTP (App Password)</p>',
     });
   }
